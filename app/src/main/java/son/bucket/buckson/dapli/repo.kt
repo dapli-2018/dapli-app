8f6f752919a3e3f63e62e460cd244c9c7c07f2e1
BucksOn <maruch3088@gmail.com>
2018-06-29 20:38:34 +0900

    add GridView with viewPager

--- app/src/main/java/son/bucket/buckson/dapli/repo.kt
+++ app/src/main/java/son/bucket/buckson/dapli/repo.kt
@@ -6,21 +6,21 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.music_list.*
+import kotlinx.android.synthetic.main.archive_list.*
 
 class repo : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         Log.d("HOME", "createview")
-        return inflater!!.inflate(R.layout.music_list, null)
+        return inflater!!.inflate(R.layout.archive_list, null)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         Log.d("HOME", "adapter")
-        var musicAdapter = ListAdapter(context, DataService.MusicData)
-        myListView.adapter = musicAdapter
+        var gridAdapter = GridAdapter(context, GridService.GridData)
+        myGridView.adapter = gridAdapter
         Log.d("HOME", "add adapter")
     }
 }
