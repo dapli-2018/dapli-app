e11ef3fb354a93e6ee0557b11a2bee174c7d42ec
BucksOn <maruch3088@gmail.com>
2018-06-29 16:48:29 +0900

    Add feedList XML:

--- app/src/main/java/son/bucket/buckson/dapli/FeedData.kt
+++ app/src/main/java/son/bucket/buckson/dapli/FeedData.kt
@@ -1,2 +1,13 @@
 package son.bucket.buckson.dapli
 
+class FeedData(val title: String, val text: String, val photo: String)
+
+object FeedService{
+    val FeedData = arrayListOf(
+            FeedData("LATATA", "(girl)IDLE", "Jacket_latata"),
+            FeedData("Marvin Gaye", "Charlie Puth", "marvingaye"),
+            FeedData("MIC DROP", "BTS", "Jacket_micdrop"),
+            FeedData("Stars", "ROTHY", "Jacket_stars"),
+            FeedData("DDU-DU DDU-DU", "BLACK PINK", "Jacket_ddududdudu")
+    )
+}
---
+++ app/src/main/java/son/bucket/buckson/dapli/list.kt
@@ -0,0 +1,26 @@
+package son.bucket.buckson.dapli
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.music_list.*
+
+class list : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        Log.d("HOME", "createview")
+        return inflater!!.inflate(R.layout.music_list, null)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        Log.d("HOME", "adapter")
+        var musicAdapter = ListAdapter(context, DataService.MusicData)
+        myListView.adapter = musicAdapter
+        Log.d("HOME", "add adapter")
+    }
+}
